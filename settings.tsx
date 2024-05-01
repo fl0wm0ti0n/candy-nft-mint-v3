3b79d37f2dc0e61875d338a6efc5aad60e024d1c
omniflow <flow@p0cki.net>
2024-05-01 23:06:23 +0200

    Update settings.tsx
    text and picture

--- settings.tsx
+++ settings.tsx
@@ -1,14 +1,12 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
-  { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
-  { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
+  { label: "VIP", mintText: "VIP mint paying with SOL", buttonLabel: "Mint now!", header: "VIP Mint" },
+  { label: "public", mintText: "Mint Paying with SOL", buttonLabel: "Mint now!", header: "Public Mint" },
 ];
 
 //header image in the ui. replace with your own
-export const image = "https://avatars.githubusercontent.com/u/93528482?v=4";
+export const image = "https://dd.dexscreener.com/ds-data/tokens/solana/97oWtQfbZMdDbn1jdNciDHm2vnPBR8VT3Ns7vSS7i9cm.png?size=lg&key=41aabf";
 
 //website title
-export const headerText = "MarkSackerberg's mint UI";
+export const headerText = "Mint Pitalik";
