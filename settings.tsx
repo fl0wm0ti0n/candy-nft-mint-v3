24d0fb7b61c2ad02e16e6c0b999240d613ab93e4
omniflow <flow@p0cki.net>
2024-05-01 23:08:18 +0200

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -2,7 +2,7 @@
 // label has to be the exact same as in your candy machine config
 export const mintText = [
   { label: "VIP", mintText: "VIP mint paying with SOL", buttonLabel: "Mint now!", header: "VIP Mint" },
-  { label: "public", mintText: "Mint Paying with SOL", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "Public", mintText: "Mint Paying with SOL", buttonLabel: "Mint now!", header: "Public Mint" },
 ];
 
 //header image in the ui. replace with your own
